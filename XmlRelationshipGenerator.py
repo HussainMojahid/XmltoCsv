e37d2592dfc9602e20d855278996ee9701390692
HussainMojahid <mojahidhussain2001@gmail.com>
2023-06-11 14:50:16 +0530

    updated Code

--- XmlRelationshipGenerator.py
+++ XmlRelationshipGenerator.py
@@ -1,26 +1,91 @@
 
-import xml.etree.ElementTree as ET
+from xml.dom import minidom
+from datetime import datetime
+import random
 
-def add_primary_key(root, primary_key=1):
-    root.set('primary_key', str(primary_key))
-    for child in root:
-        primary_key += 1
-        add_primary_key(child, primary_key)
 
-def add_foreign_key(root):
-    for element in root.iter():
-        for child in element:
-            foreign_key = element.get('primary_key')
-            if foreign_key is not None:
-                child.set('foreign_key', foreign_key)
+class XMLKeyAdder:
+    @staticmethod
+    def main():
+        xml_file_path = "input.xml"
+        XMLKeyAdder.add_keys_to_xml(xml_file_path)
 
-def add_keys_to_xml(file_path):
-    tree = ET.parse(file_path)
-    root = tree.getroot()
-    add_primary_key(root)
-    add_foreign_key(root)
-    tree.write(file_path)
+    @staticmethod
+    def add_keys_to_xml(file_path):
+        try:
+            # Create a minidom object to parse the XML file
+            doc = minidom.parse(file_path)
 
-# Usage
-xml_file = "ts.xml"
-add_keys_to_xml(xml_file)
+            # Check if a <businessId> tag is present
+            business_id = XMLKeyAdder.get_business_id(doc)
+            if business_id:
+                # Call the methods to add primary and foreign keys
+                XMLKeyAdder.add_primary_and_foreign_keys(
+                    doc.documentElement, business_id, None, None)
+
+                # Write the modified document back to the file
+                with open(file_path, "w") as file:
+                    doc.writexml(file, encoding="utf-8")
+
+        except Exception as e:
+            print(e)
+
+    @staticmethod
+    def get_business_id(doc):
+        # Check if a <businessId> tag is present
+        business_id_elements = doc.getElementsByTagName("businessId")
+        if business_id_elements:
+            business_id_element = business_id_elements[0]
+            return business_id_element.firstChild.nodeValue
+
+        return None
+
+    @staticmethod
+    def add_primary_and_foreign_keys(element, business_id, parent_key, grandparent_key):
+        # Generate the primary key as current date and time along with a random integer between 10 and 100
+        now = datetime.now().strftime("%Y%m%d%H%M%S")
+        random_key = str(random.randint(10, 100))
+        primary_key = now + random_key
+
+        # Set the 'primary_key' attribute of the current element
+        element.setAttribute("primary_key", primary_key)
+
+        # If a businessId is present, set the 'businessId' attribute of the current element
+        if business_id:
+            element.setAttribute("businessId", business_id)
+
+        # If the child tag occurs more than once, set the foreign key of the child tag to point to the great-grandparent
+        if XMLKeyAdder.has_multiple_occurrences(element):
+            element.setAttribute("foreign_key", grandparent_key)
+        # If the child tag occurs only once, set the foreign key of the element to point to its immediate parent tag
+        else:
+            element.setAttribute("foreign_key", parent_key)
+
+        # Process child elements recursively
+        children = element.childNodes
+        for child in children:
+            if child.nodeType == child.ELEMENT_NODE:
+                # Recursively call add_primary_and_foreign_keys for each child element
+                XMLKeyAdder.add_primary_and_foreign_keys(
+                    child, business_id, element.getAttribute("primary_key"), parent_key)
+
+    @staticmethod
+    def has_multiple_occurrences(element):
+        tag_name = element.tagName
+        parent_node = element.parentNode
+        count = 0
+
+        # Count the number of occurrences of the element's tag name within its parent
+        siblings = parent_node.childNodes
+        for sibling in siblings:
+            if sibling.nodeType == sibling.ELEMENT_NODE and sibling.nodeName == tag_name:
+                count += 1
+                if count > 1:
+                    return True
+
+        return False
+
+
+# Entry point
+if __name__ == "__main__":
+    XMLKeyAdder.main()
--- XmlToCsvConvertor.py
+++ XmlToCsvConvertor.py
@@ -1,4 +1,3 @@
-
 import csv
 import xml.etree.ElementTree as ET
 
@@ -8,35 +7,56 @@ def convert_xml_to_csv(xml_file):
     root = tree.getroot()
 
     def process_parent_tag(parent_tag):
-        csv_file_name = parent_tag + ".csv"
-
         child_tags = set()
-        attributes = set()
+        attribute_names = set()
 
         for element in root.iter(parent_tag):
             child_tags.update(child.tag for child in element)
-            attributes.update(element.attrib.keys())
+            attribute_names.update(element.attrib.keys())
+
+        column_names = list(attribute_names) + list(child_tags)
 
         data = []
-        non_blank_columns = list(child_tags) + list(attributes)
-
         for element in root.iter(parent_tag):
             row_data = []
-            for child_tag in child_tags:
-                child_element = element.find(child_tag)
-                row_data.append(child_element.text.strip() if child_element is not None and child_element.text is not None else "")
+            has_direct_value = False  # Flag to check if element has direct value
+            for column_name in column_names:
+                if column_name in attribute_names:
+                    row_data.append(element.attrib.get(
+                        column_name, "").strip())
+                else:
+                    child_element = element.find(column_name)
+                    if child_element is not None and child_element.text is not None:
+                        row_data.append(child_element.text.strip())
+                        has_direct_value = True
+                    else:
+                        row_data.append("")
+            if has_direct_value:  # Only append rows with direct values
+                data.append(row_data)
 
-            for attribute in attributes:
-                row_data.append(element.attrib.get(attribute, ""))
+        non_blank_columns = [column for column in column_names if any(
+            row[column_names.index(column)] != "" for row in data)]
 
-            data.append(row_data)
+        # Check if non-blank columns contain only primary key and foreign key columns
+        # Replace with actual primary key column names
+        primary_key_columns = set(
+            ["primary_key_column1", "primary_key_column2"])
+        # Replace with actual foreign key column names
+        foreign_key_columns = set(
+            ["foreign_key_column1", "foreign_key_column2"])
+        if set(non_blank_columns).issubset(primary_key_columns.union(foreign_key_columns)):
+            return
 
-        with open(csv_file_name, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(non_blank_columns)
-            writer.writerows(data)
+        filtered_data = [
+            [row[column_names.index(column)] for column in non_blank_columns] for row in data]
 
-        print(f"CSV file '{csv_file_name}' has been created successfully.")
+        if filtered_data:  # Check if filtered data has any rows
+            csv_file_name = parent_tag + ".csv"
+            with open(csv_file_name, 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(non_blank_columns)
+                writer.writerows(filtered_data)
+            print(f"CSV file '{csv_file_name}' has been created successfully.")
 
     parent_tags = set(element.tag for element in root.iter())
 
@@ -44,5 +64,5 @@ def convert_xml_to_csv(xml_file):
         process_parent_tag(parent_tag)
 
 
-xml_file_path = 'test.xml'
+xml_file_path = 'input.xml'
 convert_xml_to_csv(xml_file_path)
